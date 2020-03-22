bdcde1efc619abfaea408b83ae3652db8e2faa7a
Jonathan Tran <jonathan.t79@hotmail.com>
2020-03-22 19:22:37 +0100

    Fully and improved Program

---
+++ Restaurant/Food.cs
@@ -0,0 +1,9 @@
+﻿namespace Restaurant
+{
+    class Food
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int Price { get; set; }
+    }
+}
---
+++ Restaurant/Start.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Restaurant
+{
+    class Start
+    {
+        public void BeginProgram()
+        {
+            Restaurant r = new Restaurant();
+
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.WriteLine("Welcome to restaurant Vegetarian Fresh");
+            Console.WriteLine("Here is our digital menu:");
+            Console.WriteLine("");
+            Console.ResetColor();
+            r.ShowMenu();
+            r.UserDecision();
+        }
+    }
+}
